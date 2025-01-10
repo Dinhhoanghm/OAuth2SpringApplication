aceb97c55fa7e833ac6907a5af569c97e53d8bd9
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2025-01-10 12:29:33 +0700

    fixed

--- init_db.sh
+++ init_db.sh
@@ -1,6 +1,4 @@
-docker run --name -p 5432:5432 postgres_db -e POSTGRES_PASSWORD=admin -d postgres
 docker exec -it postgres_db bash
 psql -h localhost -p 5432 -U admin
 CREATE SCHEMA admin
 \c admin
-run db_generate.sql
