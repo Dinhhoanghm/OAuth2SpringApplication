0962b321f7fec8bc16055da66468ff506bff4b21
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2025-01-14 18:01:12 +0700

    add

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 cd db
 docker build -f Dockerfile  -t aiv-db:v1.0.0 .
-docker run --name test -p 5433:5432 -d aiv-db:v1.0.0
+docker rm -f aivhub_db
+docker run --name aivhub_db -p 5433:5432 -d aiv-db:v1.0.0
+cd fe
+docker build -t aivhub-frontend:v1.0.0 .
+docker rm -f aivhub-frontend
+docker run -d -p 5173:8080 --name aivhub-frontend aivhub-frontend:v1.0.0
 cd ../
 docker build -f Dockerfile  -t aiv-hub:v1.0.0 .
 docker rm -f aivhub-application
